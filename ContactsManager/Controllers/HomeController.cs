2fd011878927c8e151a41b0ece6f0c88cd39c132
Kapil Upreti <kapilupretiff@gmail.com>
2025-10-14 13:16:24 +0545

    setup

---
+++ ContactsManager/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace ContactsManager.Controllers
+{
+    public class HomeController:Controller
+    {
+
+        [Route("/")]
+
+        public IActionResult Index()
+        {
+            return View(viewName:"Index");
+        }
+    }
+}
