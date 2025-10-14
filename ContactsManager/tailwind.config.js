c7c9540f4349162d8bf0abfe09f8c2af81359fd7
Kapil Upreti <kapilupretiff@gmail.com>
2025-10-14 14:08:55 +0545

    tailwind css setup completed

---
+++ ContactsManager/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./Pages/**/*.cshtml",
+		"./Views/**/*.cshtml",
+		"./**/*.razor",
+		"./**/*.cshtml"
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
