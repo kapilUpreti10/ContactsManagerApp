c80c767db393c8f5a52cbd78b0c819e432bb7dd7
Kapil Upreti <kapilupretiff@gmail.com>
2025-12-19 21:07:48 +0545

    initial setup for ui is done

---
+++ ContactsManager/Controllers/PersonController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsManager.Controllers
+{
+    public class PersonController:Controller
+    {
+
+        [Route("/persons/index")]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
